8f858d0d38febc650d70f21d51de419a4eee647e
Kamil Stys <kstys8@wp.pl>
2020-05-03 18:58:35 +0200

    Creating folders nedded for URLs generation

--- pages/cudzoziemcy/obywatele-państw-trzecich/[id].js
+++ pages/cudzoziemcy/obywatele-państw-trzecich/[id].js
@@ -1,6 +1,6 @@
-import FoldersHeaderBar from "../../components/shared/Cudzoziemcy/FoldersHeaderBar";
-import BaseLayout from "../../components/Layouts/BaseLayout";
-import { getAllIssuesIds, getIssueData } from "../../lib/issues";
+import FoldersHeaderBar from "../../../components/shared/Cudzoziemcy/FoldersHeaderBar";
+import BaseLayout from "../../../components/Layouts/BaseLayout";
+import { getAllIssuesIds, getIssueData } from "../../../lib/issues";
 
 export async function getStaticPaths() {
   const paths = getAllIssuesIds();
@@ -25,6 +25,7 @@ const Issue = ({ issueData }) => {
       <FoldersHeaderBar />
       <div className="grey-box issue">
         <div className="issue-content">
+          <h1 className="issue-title">{issueData.title}</h1>
           <div dangerouslySetInnerHTML={{ __html: issueData.contentHtml }} />
         </div>
       </div>
