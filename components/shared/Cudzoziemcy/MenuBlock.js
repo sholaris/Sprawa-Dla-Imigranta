4401be9ab1e6db2938a10f150963e61635b88d2d
Kamil Stys <kstys8@wp.pl>
2020-05-05 19:18:48 +0200

    Removed unnecessary attributes

--- components/shared/Cudzoziemcy/MenuBlock.js
+++ components/shared/Cudzoziemcy/MenuBlock.js
@@ -2,8 +2,8 @@ const MenuBlock = React.forwardRef(({ index, title, onClick, href }, ref) => {
   return (
     <a href={href} ref={ref} onClick={onClick} className={index}>
       <div className="block-option">
-        <div id={title} className={index}>
-          <h2 className={index}>{title}</h2>
+        <div className={index}>
+          <h2>{title}</h2>
         </div>
       </div>
     </a>
