a6f5c552aed26db6977813ce735bebb41bf3fb8b
Kamil Stys <kstys8@wp.pl>
2020-05-06 20:01:06 +0200

    Added compression of too long routes names

--- components/shared/Cudzoziemcy/PermitOptions.js
+++ components/shared/Cudzoziemcy/PermitOptions.js
@@ -1,160 +1,34 @@
 import { Container, Row, Col } from "react-bootstrap";
 import MenuBlock from "./MenuBlock";
 import Link from "next/link";
+import { pobyt_staly } from "../../../public/static/pagedata/zezwolenie";
+import { CompressLink } from "../../../lib/utils";
 
 const PermitOptions = () => {
   return (
     <Container className="grey-box permanent-residence-permit">
-      <Row className="permanent-residence-permit">
-        <Col xs={8} md={8} lg={4}>
-          <Link
-            href="/cudzoziemcy/obywatele-panstw-trzecich/zezwolenie-na-pobyt-staly/[id]"
-            passHref
-          >
-            <MenuBlock
-              index={"Podstawowe informacje"}
-              title={"Podstawowe informacje"}
-            />
-          </Link>
-        </Col>
-        <Col xs={8} md={8} lg={4}>
-          <Link
-            href={
-              "/cudzoziemcy/obywatele-panstw-trzecich/zezwolenie-na-pobyt-staly/[id]"
-            }
-            passHref
-          >
-            <MenuBlock
-              index={"Dla posiadacza ważnej karty polaka"}
-              title={"Dla posiadacza ważnej karty polaka"}
-            />
-          </Link>
-        </Col>
-      </Row>
-      <Row className="permanent-residence-permit">
-        <Col xs={8} md={8} lg={4}>
-          <Link
-            href={
-              "/cudzoziemcy/obywatele-panstw-trzecich/zezwolenie-na-pobyt-staly/[id]"
-            }
-            passHref
-          >
-            <MenuBlock
-              index={"Dla małżonka polskiego obywatela"}
-              title={"Dla małżonka polskiego obywatela"}
-            />
-          </Link>
-        </Col>
-        <Col xs={8} md={8} lg={4}>
-          <Link
-            href={
-              "/cudzoziemcy/obywatele-panstw-trzecich/zezwolenie-na-pobyt-staly/[id]"
-            }
-            passHref
-          >
-            <MenuBlock
-              index={"Dla ofiary handlu ludźmi"}
-              title={"Dla ofiary handlu ludźmi"}
-            />
-          </Link>
-        </Col>
-      </Row>
-      <Row className="permanent-residence-permit">
-        <Col xs={8} md={8} lg={4}>
-          <Link
-            href={
-              "/cudzoziemcy/obywatele-panstw-trzecich/zezwolenie-na-pobyt-staly/[id]"
-            }
-            passHref
-          >
-            <MenuBlock
-              index={"Dla cudzoziemca posiadającego polskie pochodzenie"}
-              title={"Dla cudzoziemca posiadającego polskie pochodzenie"}
-            />
-          </Link>
-        </Col>
-        <Col xs={8} md={8} lg={4}>
-          <Link
-            href={
-              "/cudzoziemcy/obywatele-panstw-trzecich/zezwolenie-na-pobyt-staly/[id]"
-            }
-            passHref
-          >
-            <MenuBlock
-              index={"Dla cudzoziemca, któremu udzielono w polsce azylu"}
-              title={"Dla cudzoziemca, któremu udzielono w polsce azylu"}
-            />
-          </Link>
-        </Col>
-      </Row>
-      <Row className="permanent-residence-permit">
-        <Col xs={8} md={8} lg={4}>
-          <Link
-            href={
-              "/cudzoziemcy/obywatele-panstw-trzecich/zezwolenie-na-pobyt-staly/[id]"
-            }
-            passHref
-          >
-            <MenuBlock
-              index={
-                "Dla cudzoziemca przebywającego w polsce na podstawie zgody na pobyt tolerowany"
-              }
-              title={
-                "Dla cudzoziemca przebywającego w polsce na podstawie zgody na pobyt tolerowany"
-              }
-            />
-          </Link>
-        </Col>
-        <Col xs={8} md={8} lg={4}>
-          <Link
-            href={
-              "/cudzoziemcy/obywatele-panstw-trzecich/zezwolenie-na-pobyt-staly/[id]"
-            }
-            passHref
-          >
-            <MenuBlock
-              index={"Dla małoletniego małżonka polskiego obywatela"}
-              title={"Dla małoletniego małżonka polskiego obywatela"}
-            />
-          </Link>
-        </Col>
-      </Row>
-      <Row className="permanent-residence-permit">
-        <Col xs={8} md={8} lg={4}>
-          <Link
-            href={
-              "/cudzoziemcy/obywatele-panstw-trzecich/zezwolenie-na-pobyt-staly/[id]"
-            }
-            passHref
-          >
-            <MenuBlock
-              index={
-                "Dla cudzoziemca przebywającego w polsce na podstawie statusu uchodźcy, ochrony uzupełniającej lub zgody na pobyt ze względów humanitarnych"
-              }
-              title={
-                "Dla cudzoziemca przebywającego w polsce na podstawie statusu uchodźcy, ochrony uzupełniającej lub zgody na pobyt ze względów humanitarnych"
-              }
-            />
-          </Link>
-        </Col>
-        <Col xs={8} md={8} lg={4}>
-          <Link
-            href={
-              "/cudzoziemcy/obywatele-panstw-trzecich/zezwolenie-na-pobyt-staly/[id]"
-            }
-            passHref
-          >
-            <MenuBlock
-              index={
-                "Dla małoletniego dziecka cudzoziemca przebywającego w polsce na podstawie zezwolenia na pobyt stały lub zezwolenia na pobyt rezydenta długoterminowego ue"
-              }
-              title={
-                "Dla małoletniego dziecka cudzoziemca przebywającego w polsce na podstawie zezwolenia na pobyt stały lub zezwolenia na pobyt rezydenta długoterminowego ue"
-              }
-            />
-          </Link>
-        </Col>
-      </Row>
+      {pobyt_staly.map((row) => {
+        return (
+          <Row className="permanent-residence-permit">
+            {row.Row.Cols.map((option) => {
+              return (
+                <Col xs={8} md={8} lg={4}>
+                  <Link
+                    href="/cudzoziemcy/obywatele-panstw-trzecich/zezwolenie-na-pobyt-staly/[id]"
+                    as={`/cudzoziemcy/obywatele-panstw-trzecich/zezwolenie-na-pobyt-staly/${CompressLink(
+                      option,
+                      13
+                    )}`}
+                    passHref
+                  >
+                    <MenuBlock index={option} title={option} />
+                  </Link>
+                </Col>
+              );
+            })}
+          </Row>
+        );
+      })}
     </Container>
   );
 };
