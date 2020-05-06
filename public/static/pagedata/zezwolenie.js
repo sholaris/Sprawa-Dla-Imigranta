1e0b101e6dd3f7309ec710177e0967a1809200b0
Kamil Stys <kstys8@wp.pl>
2020-05-06 19:57:49 +0200

    created objects with temporary residence permissions and permanent residence permissions

--- public/static/pagedata/zezwolenie.js
+++ public/static/pagedata/zezwolenie.js
@@ -1,14 +1,105 @@
-const zezwolenie_opcje = [
-  "podstawowe informacje",
-  "dla małoletniego polskiego obywatela",
-  "dla małżonka polskiego obywatela",
-  "dla ofiary handlu ludźmi",
-  "dla cudzoziemca posiadającego polskie pochodzenie",
-  "dla cudzoziemca, któremu udzielono w polsce azylu",
-  "dla cudzoziemca przebywającego w polsce na podstawie zgody na pobyt tolerowany",
-  "dla posiadacza ważnej karty polaka",
-  "dla cudzoziemca przebywającego w polsce na podstawie statusu uchodźcy, ochrony uzupełniającej lub zgody na pobyt ze względów humanitarnych",
-  "dla małoletniego dziecka cudzoziemca przebywającego w polsce na podstawie zezwolenia na pobyt stały lub zezwolenia na pobyt rezydenta długoterminowego ue",
+export const pobyt_staly = [
+  {
+    Row: {
+      Cols: ["podstawowe informacje", "dla posiadacza ważnej karty polaka"],
+    },
+  },
+  {
+    Row: {
+      Cols: ["dla małżonka polskiego obywatela", "dla ofiary handlu ludźmi"],
+    },
+  },
+  {
+    Row: {
+      Cols: [
+        "dla cudzoziemca posiadającego polskie pochodzenie",
+        "dla cudzoziemca, któremu udzielono w polsce azylu",
+      ],
+    },
+  },
+  {
+    Row: {
+      Cols: [
+        "dla cudzoziemca przebywającego w polsce na podstawie zgody na pobyt tolerowany",
+        "dla małoletniego dziecka polskiego obywatela",
+      ],
+    },
+  },
+  {
+    Row: {
+      Cols: [
+        "dla cudzoziemca przebywającego w polsce na podstawie statusu uchodźcy, ochrony uzupełniającej lub zgody na pobyt ze względów humanitarnych",
+        "dla małoletniego dziecka cudzoziemca przebywającego w polsce na podstawie zezwolenia na pobyt stały lub zezwolenia na pobyt rezydenta długoterminowego ue",
+      ],
+    },
+  },
 ];
 
-export default zezwolenie_opcje;
+export const pobyt_czasowy = {
+  dane: [
+    {
+      tytul: "Podstawowe informacje",
+      opcje: [
+        "Podstawowe informacje dotyczące uzyskania zezwolenia na pobyt czasowy",
+      ],
+    },
+    {
+      tytul: "Praca",
+      opcje: [
+        "Jednolite zezwolenie na pobyt i pracę",
+        "Praca w zawodzie wymagającym wysokich kwalifikacji",
+        "Pracownik delegowany",
+        "Praca sezonowa",
+        "Wykonywanie pracy w trakcie postępowania o udzielenie zezwolenia na pobyt czasowy",
+      ],
+    },
+    {
+      tytul: "Działalność gospodarcza",
+      opcje: ["Zezwolenie w celu prowadzenia działalność gospodarcza"],
+    },
+    {
+      tytul: "Studia i nauka",
+      opcje: [
+        "Zezwolenie w celu kształcenia się na studia",
+        "Zezwolenie w celu odbywania nauki",
+        "Absolwent poszukujący pracy lub lub planujący wykonywanie działalności gospodarczej",
+      ],
+    },
+    {
+      tytul: "Stażysta",
+      opcje: ["Zezwolenie w celu odbyca stażu"],
+    },
+    {
+      tytul: "Wolontariusz",
+      opcje: ["Zezwolenie w celu udziału w wolontariacie"],
+    },
+    {
+      tytul: "Pobyt z rodziną",
+      opcje: [
+        "Pobyt z polskim obywatelem",
+        "Pobyt z obywatelem państwa trzeciego",
+        "Pobyt z obywatelem UE/EOG/Szwajcarii",
+      ],
+    },
+    {
+      tytul: "Naukowiec",
+      opcje: [
+        "Zezwolenie w celu prowadzenia badań naukowych",
+        "Naukowiec poszukujący pracy lub planujący wykonywanie działalności gospodarczej",
+      ],
+    },
+    {
+      tytul: "Ofiara handlu ludźmi",
+      opcje: ["Zezwolenie dla ofiary handlu ludźmi"],
+    },
+
+    {
+      tytul: "Pobyt z uwagi na wyjątkowe okoliczności",
+      opcje: [
+        "Okoliczności wymagające krótkotrwałego pobytu",
+        "Konieczność poszanowania do życia codziennego",
+        "Koniecznośc ochrony praw dziecka",
+      ],
+    },
+  ],
+};
