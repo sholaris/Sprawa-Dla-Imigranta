e9c91c38d16fb686d20ffd111bc348510d7f2c00
Kamil Stys <kstys8@wp.pl>
2020-05-06 19:53:04 +0200

    Handle new page with cards options

--- components/shared/Cudzoziemcy/Folders.js
+++ components/shared/Cudzoziemcy/Folders.js
@@ -2,6 +2,7 @@ import Folder from "./Folder";
 import { Container } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import katalogi from "../../../public/static/pagedata/katalogi";
+import { pobyt_czasowy } from "../../../public/static/pagedata/zezwolenie";
 
 const Folders = (props) => {
   const [issues, setIssues] = useState([]);
@@ -17,14 +18,17 @@ const Folders = (props) => {
       setIssues(katalogi.obywatel_wb);
     } else if (props.type == "obywatele_pt") {
       setIssues(katalogi.obywatel_pt);
+    } else if (props.type == "pobyt_czasowy") {
+      setIssues(pobyt_czasowy.dane);
     }
   };
 
   return (
-    <Container className="grey-box folders">
+    <Container className={`grey-box folders ${props.type}`}>
       <h2>{props.header}</h2>
       {issues.map((issue) => (
         <Folder
+          type={props.type}
           category={props.header}
           title={issue.tytul}
           options={issue.opcje}
